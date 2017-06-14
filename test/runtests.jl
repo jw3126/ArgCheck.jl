b3a93991e900b3f978d03eeebb4ecd0f6b7c9455
Jan Weidner <jw3126@gmail.com>
2017-06-14 10:27:20 +0200

    Better error messages for comparisons.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,52 @@
 using ArgCheck
 using Base.Test
+using Compat
 import ArgCheck: build_error
-x = 1
-@test_throws ArgumentError (@argcheck x > 1)
-@test_throws ArgumentError @argcheck x > 1 "this should not happen"
-@argcheck x>0 # does not throw
 
-n =2; m=3
-@test_throws DimensionMismatch (@argcheck n==m DimensionMismatch)
-@argcheck n==n DimensionMismatch
+import ArgCheck: iscomparison
+@testset "iscomparison" begin
+    @test iscomparison(:(1==2))
+    @test iscomparison(:(f(2x) + 1 ≈ f(x)))
+    @test iscomparison(:(<(2,3)))
+    @test !iscomparison(:(f(1,1)))
+end
 
-denominator = 0
-@test_throws DivideError (@argcheck denominator != 0 DivideError())
-@argcheck 1 !=0 DivideError()
+@testset "@argcheck" begin
+    @test_throws ArgumentError @argcheck false
+    @argcheck true
+    
+    x = 1
+    @test_throws ArgumentError (@argcheck x > 1)
+    @test_throws ArgumentError @argcheck x > 1 "this should not happen"
+    @argcheck x>0 # does not throw
+    
+    n =2; m=3
+    @test_throws DimensionMismatch (@argcheck n==m DimensionMismatch)
+    @argcheck n==n DimensionMismatch
+    
+    denominator = 0
+    @test_throws DivideError (@argcheck denominator != 0 DivideError())
+    @argcheck 1 !=0 DivideError()
+end
+
+@compat struct MyError <: Exception
+    msg::String
+end
+
+@testset "error message" begin
+    x = 1.23455475675
+    y = 2.345345345
+    # comparison
+    try
+        @argcheck x == y MyError
+        @test false # argcheck shout throw before this is reached
+    catch err
+        @test isa(err, MyError)
+        msg = err.msg
+        @test contains(msg, string(x))
+        @test contains(msg, string(y))
+        @test contains(msg, "x")
+        @test contains(msg, "y")
+        @test contains(msg, "==")
+    end
+end
