aa0403279dc680ab957a2a14fac1c4ed20e4760e
Jan Weidner <jw3126@gmail.com>
2018-06-21 20:57:17 +0200

    fix

--- src/ArgCheck.jl
+++ src/ArgCheck.jl
@@ -1,10 +1,6 @@
 __precompile__()
 module ArgCheck
 
-if VERSION < v"0.7-"
-    pushfirst!(args...) = unshift!(args...)
-end
-
 using Base.Meta
 export @argcheck, @check, CheckError
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,7 @@
 using ArgCheck
-if VERSION < v"0.7.0-"
-    using Base.Test
-else
-    using Test
-    using Random
-    contains(haystick, needle) = occursin(needle, haystick)
-end
+
+using Test
+using Random
 
 include("checks.jl")
 include("perf.jl")
