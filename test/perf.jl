0b1954565c685a1017c9715e8f44f0afafc4a07d
Jiayi Wei <MrVPlusOne@gmail.com>
2022-02-05 15:26:39 -0600

    Fix performance tests (prevent inlining).

--- test/perf.jl
+++ test/perf.jl
@@ -3,24 +3,24 @@ module Perf
 using BenchmarkTools
 using ArgCheck
 
-truthy(x) = true
+@noinline truthy(x) = x == x
 
-function fallback_argcheck(x)
+@noinline function fallback_argcheck(x)
     @argcheck x
 end
-function comparison_argcheck(x)
+@noinline function comparison_argcheck(x)
     @argcheck x == x
 end
-function call_argcheck(x)
+@noinline function call_argcheck(x)
     @argcheck truthy(x)
 end
-function fallback_assert(x)
+@noinline function fallback_assert(x)
     @assert x
 end
-function comparison_assert(x)
+@noinline function comparison_assert(x)
     @assert x == x
 end
-function call_assert(x)
+@noinline function call_assert(x)
     @assert truthy(x)
 end
 
@@ -30,11 +30,11 @@ benchmarks =[
     (comparison_assert, comparison_argcheck, 42),
     ]
 
-for (f_argcheck, f_assert, arg) in benchmarks
-    println(f_argcheck)
-    @btime ($f_argcheck)($arg)
+for (f_assert, f_argcheck, arg) in benchmarks
     println(f_assert)
     @btime ($f_assert)($arg)
+    println(f_argcheck)
+    @btime ($f_argcheck)($arg)
 end
 
 end#module
