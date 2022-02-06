197e6155704ff40633517c9cc480c6a690fb1c90
Jiayi Wei <MrVPlusOne@gmail.com>
2022-02-06 16:23:57 -0600

    Support subtype checks (#42)
    * More performance tests (prevent inlining).
    
    * Support subtype checks.
    
    * Bump version.

--- test/perf.jl
+++ test/perf.jl
@@ -3,38 +3,46 @@ module Perf
 using BenchmarkTools
 using ArgCheck
 
-truthy(x) = true
+@noinline truthy(x) = x == x
+truthy2(x) = true
 
-function fallback_argcheck(x)
+@noinline function fallback_argcheck(x)
     @argcheck x
 end
-function comparison_argcheck(x)
+@noinline function comparison_argcheck(x)
     @argcheck x == x
 end
-function call_argcheck(x)
+@noinline function call_argcheck(x)
     @argcheck truthy(x)
 end
-function fallback_assert(x)
+function call_argcheck2(x)
+    @argcheck truthy2(x)
+end
+@noinline function fallback_assert(x)
     @assert x
 end
-function comparison_assert(x)
+@noinline function comparison_assert(x)
     @assert x == x
 end
-function call_assert(x)
+@noinline function call_assert(x)
     @assert truthy(x)
+end
+function call_assert2(x)
+    @assert truthy2(x)
 end
 
 benchmarks =[
     (fallback_assert, fallback_argcheck, true),
     (call_assert, call_argcheck, 42),
+    (call_assert2, call_argcheck2, 42),
     (comparison_assert, comparison_argcheck, 42),
     ]
 
-for (f_argcheck, f_assert, arg) in benchmarks
-    println(f_argcheck)
-    @btime ($f_argcheck)($arg)
+for (f_assert, f_argcheck, arg) in benchmarks
     println(f_assert)
     @btime ($f_assert)($arg)
+    println(f_argcheck)
+    @btime ($f_argcheck)($arg)
 end
 
 end#module
